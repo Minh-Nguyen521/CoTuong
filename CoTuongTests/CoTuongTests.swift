73efd620e47ec97a91ae6703e2a63f6454b726f0
Minh-Nguyen521 <minhb2306674@student.ctu.edu.vn>
2025-01-28 11:25:03 +0700

    feat: add test

---
+++ CoTuongTests/CoTuongTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoTuongTests.swift
+//  CoTuongTests
+//
+//  Created by Minh on 26/1/25.
+//
+
+import Testing
+
+struct CoTuongTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
